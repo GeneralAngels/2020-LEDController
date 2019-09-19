a39f6c9884e3be34c741f111dd426257d0777be6
General Angels <wegotanotheremail@gmail.com>
2019-09-19 12:50:19 +0300

    Initial

--- 2020-LEDController.ino
+++ 2020-LEDController.ino
@@ -1,9 +1,54 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <FastLED.h>
 
+#define LED_PIN 3
+#define LED_COUNT 38
+#define LOOP_DELAY 50 //50Hz
+
+CRGB leds[LED_COUNT];
+
+byte packet[256];
+int availableBytes;
+
+long i;
+CRGB color;
+
+void setup() {
+  Serial.begin(9600);
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, LED_COUNT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int availableBytes = Serial.available();
+  if (availableBytes > 0) {
+    Serial.readBytes(packet, availableBytes);
+    switch (packet[0]) {
+      case 0x00:
+        eraseAll();
+        break;
+      case 0x01:
+        pushLED();
+        break;
+      case 0x02:
+        overwriteLEDs();
+        break;
+      case 0x03:
+        break;
+    }
+    FastLED.show();
+  }
+}
+
+void eraseAll() {
+  color = CRGB(0, 0, 0);
+  for (i = 0; i < LED_COUNT; i++) {
+    leds[i] = color;
+  }
+}
+
+void pushLED() {
+  color = CRGB(0, 0, 0);
+}
+
+void overwriteLEDs() {
 
 }
